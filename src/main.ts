0feb69bec009b5885ee51d91f27b56b5c1c58f12
lallemand-fred <lallemand_fred@hotmail.com>
2025-09-09 22:06:42 +0200

    Add: folder,fille,module,controler feature/account

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './feature/main/app.module';
+import { AppModule } from './feature/main';
 
-async function bootstrap() {
+const bootstrap = async (): Promise<void> => {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+  //process.env.PORT ?? 3000 = Si la valeur process.env.PORT n’existe pas, ?? = alors prends celle-ci : 3000.
 }
 bootstrap().then();
