136be02aa0b93b53f5076880e3157bc969593cab
lallemand-fred <lallemand_fred@hotmail.com>
2025-09-12 22:48:13 +0200

    Add: account.entity.ts and app.module.ts 'Branchement entre application et DB'

--- src/feature/main/app.module.ts
+++ src/feature/main/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from '../account/account.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configManager } from '@common';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule,TypeOrmModule.forRoot(configManager.getTypeOrmConfig())],
   controllers: [AppController], // Ajoutez votre nouveau contrôleur ici
   providers: [AppService],
 })
