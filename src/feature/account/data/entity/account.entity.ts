47c2f808d82878b19443349b9e5b63abb93b1548
lallemand-fred <lallemand_fred@hotmail.com>
2025-09-13 15:48:50 +0200

    Add: @Entity du file account.entity.ts

--- src/feature/account/data/entity/account.entity.ts
+++ src/feature/account/data/entity/account.entity.ts
@@ -1,7 +1,21 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
+import { ulid } from 'ulid';
+
+@Entity()
 export class Account{
+  @PrimaryColumn('varchar', { length:26, default: () => `'${ulid()}'` })
   account_id: string;
+  @Column()
   firstname: string;
+  @Column()
   lastname: string;
+  @Column()
   birthdate: Date;
+  @Column()
   mail: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 }
