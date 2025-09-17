eada117a616cecee785e5d556bb0b6bc2ead12c4
lallemand-fred <lallemand_fred@hotmail.com>
2025-09-17 11:12:11 +0200

    Add: exception/security.exception.ts up file api-code-response.enum.ts add new keys

---
+++ src/feature/security/jwt/token.service.ts
@@ -0,0 +1,73 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  Credentials,
+  RefreshPayload,
+  Token,
+  TokenExpiredException,
+  TokenGenerationException,
+  UserNotFoundException,
+} from '../data';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigKey, configManager } from '@common';
+import { Builder } from 'builder-pattern';
+import { isNil } from 'lodash';
+
+@Injectable()
+export class TokenService {
+  private readonly logger = new Logger(TokenService.name);
+  constructor(
+    @InjectRepository(Token) private readonly repository: Repository<Token>,
+    @InjectRepository(Credentials)
+    private readonly credentialRepository: Repository<Credential>,
+    private jwtService: JwtService,
+  ) {}
+
+  async getTokens(credential: Credentials): Promise<Token | null> {
+    try {
+      await this.repository.delete({ credential });
+      const payload = { sub: credential.credential_id };
+      const token = await this.jwtService.signAsync(payload, {
+        secret: configManager.getValue(ConfigKey.JWT_TOKEN_SECRET),
+        expiresIn: configManager.getValue(ConfigKey.JWT_TOKEN_EXPIRE_IN),
+      });
+      const refreshToken = await this.jwtService.signAsync(payload, {
+        secret: configManager.getValue(ConfigKey.JWT_REFRESH_TOKEN_SECRET),
+        expiresIn: configManager.getValue(
+          ConfigKey.JWT_REFRESH_TOKEN_EXPIRE_IN,
+        ),
+      });
+      await this.repository.upsert(
+        Builder<Token>()
+          .token(token)
+          .refreshToken(refreshToken)
+          .credential(credential)
+          .build(),
+        ['credential'],
+      );
+      return await this.repository.findOneBy({ token: token });
+    } catch (e) {
+      this.logger.error(e.message);
+      throw new TokenGenerationException();
+    }
+  }
+  async deleteFor(credential: Credentials): Promise<void> {
+    await this.repository.delete({ credential });
+  }
+  async refresh(payload: RefreshPayload): Promise<Token | null> {
+    try {
+      const id = this.jwtService.verify(payload.refresh, {
+        secret: configManager.getValue(ConfigKey.JWT_REFRESH_TOKEN_SECRET)}).sub;
+      // @ts-ignore
+      const credential: Credentials | null = await this.credentialRepository.findOneBy({ credential_id: id });
+      if (isNil(credential)) {
+        throw new UserNotFoundException();
+      }
+      return await this.getTokens(credential);
+    } catch (e) {
+      this.logger.error(e.message);
+      throw new TokenExpiredException();
+    }
+  }
+}
---
+++ src/feature/security/security.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Credentials, Token } from './data/entity';
+import { TokenService } from './jwt/token.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigKey, configManager } from '@common';
+
+
+@Module({
+  imports: [JwtModule.register({
+    global:true,
+    secret: configManager.getValue(ConfigKey.JWT_TOKEN_SECRET),
+    signOptions: {expiresIn: configManager.getValue(ConfigKey.JWT_TOKEN_EXPIRE_IN)},
+  }),TypeOrmModule.forFeature([Credentials,Token])],
+  providers: [TokenService],
+})
+export class SecurityModule {}
